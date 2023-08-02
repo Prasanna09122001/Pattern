b481e8d93000be7f9594ecc5e98b3e299822b40f
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-08-02 13:05:56 +0530

    [Prasanna] Add: Implement the Regex Email Program

--- Pattern/Pattern/Program.cs
+++ Pattern/Pattern/Program.cs
@@ -6,22 +6,21 @@ namespace pattern
     { 
         static void Main()
         {
-             ValidatePincode validatepincode = new ValidatePincode();
+            /* ValidatePincode validatepincode = new ValidatePincode();
              validatepincode.UC1_Validate("400088");
              validatepincode.UC2_Validate("A400088");
              validatepincode.UC3_Validate("400088B");
              validatepincode.UC4_Validate("400 088");
+                              "^[A-Z]{1,}[a-z]{5,}[!@#%&*~$]{1}[0-9]{1,}$";
 
-
-            /*
-           ValidateEmail validateEmail = new ValidateEmail();
-           validateEmail.UC1_Validate("abc");
-           validateEmail.UC2_Validate("@bridgelabz");
-           validateEmail.UC3_Validate(".co");
-           validateEmail.UC4_Validate("abcd.xyz");
-           validateEmail.ActualValidate("abc@bridgelabz.co.in");
-             "^[A-Z]{1,}[a-z]{5,}[!@#%&*~$]{1}[0-9]{1,}$";
-           */
+             */
+            ValidateEmail validateEmail = new ValidateEmail();
+            validateEmail.UC1_Validate("abc");
+            validateEmail.UC2_Validate("@bridgelabz");
+            validateEmail.UC3_Validate(".co");
+            validateEmail.UC4_Validate("abcd.xyz");
+            validateEmail.ActualValidate("abc@bridgelabz.co.in");
+            
         }
     }
 }
